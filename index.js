14eb4f4a55d84218c407f34efd99ebe606e8a8be
RUBANGAKENE BRIAN DAVID <149721916+Rubangaken@users.noreply.github.com>
2024-02-07 19:26:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let results;
+function validation (password, email){
+if (email == "musinguzibrian@gmail.com" || password == "2200706408@2024")
+console.log(`User email is ${email}, login successful`)
+else 
+console.log(`incorrect user credentials`)
+    // return results;
+}
+
+validation("2200706408@2024","musinguzibrian@gmail.com");
