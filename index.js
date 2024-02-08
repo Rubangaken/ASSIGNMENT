8bd15d61bc2111c80dd0c732052047be2783a50e
RUBANGAKENE BRIAN DAVID <149721916+Rubangaken@users.noreply.github.com>
2024-02-08 10:58:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let results;
 function validation (password, email){
-if (email == "musinguzibrian@gmail.com" || password == "2200706408@2024")
+if (email == "rubangakenebriandavid8@gmail.com" || password == "2200706849@2024")
 console.log(`User email is ${email}, login successful`)
 else 
 console.log(`incorrect user credentials`)
