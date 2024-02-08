a38e06967395580defde8135a12aade26b0f73f7
RUBANGAKENE BRIAN DAVID <149721916+Rubangaken@users.noreply.github.com>
2024-02-08 10:57:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ if (email == "musinguzibrian@gmail.com" || password == "2200706408@2024")
 console.log(`User email is ${email}, login successful`)
 else 
 console.log(`incorrect user credentials`)
-    // return results;
 }
 
-validation("2200706408@2024","musinguzibrian@gmail.com");
+validation("2200706849@2024","rubangakenebriandavid8@gmail.com");
